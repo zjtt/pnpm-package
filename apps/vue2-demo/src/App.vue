cfa294e610ead9802a0e5dedf6b5c1b0c072b15a
zhenjintao <zhenjintao@chinaso.com>
2023-04-11 15:09:05 +0800

    feat: apps

---
+++ apps/vue2-demo/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <Diycategory>
+      <template scope="test">
+        {{ JSON.stringify(test) }}
+        <div>使用template标签+scope接收</div>
+      </template>
+    </Diycategory>
+    <img
+      @click="cpText(url)"
+      width="20"
+      src="https://blog-huahua.oss-cn-beijing.aliyuncs.com/blog/code/copy.png"
+    />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import cpText from "@zh-achieve/copy"
+import Diycategory from "./components/category.vue"
+export default {
+  name: "App",
+  data() {
+    return {
+      url: "66666666"
+    }
+  },
+  methods: {
+    cpText
+  },
+  components: {
+    // HelloWorld,
+    Diycategory
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ apps/vue2-demo/src/components/category.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="category">
+    <slot :games="games" test="测试" msg="加油,你是最棒的">我是默认的内容</slot>
+  </div>
+</template>
+
+<script>
+// 作用域插槽就是父组件在调用子组件的时候给子组件传了一个插槽，这个插槽为作用域插槽，
+// 该插槽必须放在template标签里面，同时声明从子组件接收的数据放在一个自定义属性内，
+// 并定义该数据的渲染方式，实现从子组件到父组件的数据传递。
+export default {
+  name: "DiyCategory",
+  props: ["title"],
+  data() {
+    return {
+      games: ["红色警戒", "穿越火线", "劲舞团", "超级玛丽"],
+    };
+  },
+};
+</script>
